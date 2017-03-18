de38b19242a5f4e987154deabec82e720fa63110
RainCoder <jyolainnie_daniel@hotmail.com>
2017-03-18 04:15:53 -0400

    Added methods

--- StackLL.cpp
+++ StackLL.cpp
@@ -1,1 +1,53 @@
 #include "StackLL.h"
+#include <iostream>
+using namespace std;
+
+
+class List::Node //self-referential Node class
+{
+	public:
+	   int data = 0;
+	   Node* link = nullptr;
+	
+	};
+
+Stack::~Stack()
+{
+while(num_elements > 0)
+ remove(1);
+}
+
+int Stack::size()
+{
+	return num_elements;
+}
+
+void Stack::push(int val)
+{
+	Node* newPtr = new Node{val};
+	newPtr -> link = frontPtr;
+	frontPtr = newPtr;
+	
+	num_element++;
+}
+
+void Stack::pop()
+{
+	Node* delPtr;
+	delPtr = frontPtr;
+	frontPtr = frontPtr -> link;
+	
+	delete delPtr;
+	num_element--;
+}
+
+int Stack::top()
+{
+	return frontPtr -> data;
+}
+
+void Stack::clear()
+{
+	while(num_elements != 0)
+	 int x = remove(1);
+}
