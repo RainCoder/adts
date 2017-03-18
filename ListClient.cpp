3f976fc68022ea584e2c721d374ee7aecf988b54
RainCoder <jyolainnie_daniel@hotmail.com>
2017-03-18 03:02:23 -0400

    more corrections

--- ListClient.cpp
+++ ListClient.cpp
@@ -11,13 +11,13 @@ int main()
 int lim, rm;
 char ans;
 
-cout << "Please enter list size (an integer)";
+cout << "Please enter list size (an integer) ";
 cin >> lim;
 
 
-for ( int i= 1; i >= lim ; i++)
+for ( int i= 1; i <= lim ; i++)
 {
-	if(i ==1)
+	if(i == 1)
 	{
 		L1.insert(i+1, i);
 		L2.insert(i+2, i);
@@ -29,47 +29,56 @@ for ( int i= 1; i >= lim ; i++)
 
 cout << "List 1: ";
 L1.display();
-cout << "size: " << L1.size();
+cout << "\n";
+cout << "size: " << L1.size() << "\n";
 
 cout << "List 2: ";
 L2.display();
-cout << "size: " << L2.size();
+cout << "\n";
+cout << "size: " << L2.size() << "/n";
 
 cout << "Do you wish to remove an element in List 1? (y/n)";
 cin >> ans;
 
 while( ans == 'y' && L1.size() != 0){
-
+cout << "\n";
 cout << "Please enter the position of the element you wish to remove in List 1(an integer)";
 cin >> rm;
 
 L1.remove(rm);
 
+cout << "\n";
 cout << "List 1: ";
 L1.display();
-cout << "size: " << L1.size();
+cout << "\n";
+cout << "size: " << L1.size() << "\n";
 
 cout << "Do you wish to remove another element in List 1? (y/n)";
 cin >> ans;
 }
 
-while( ans == 'y' && L2.size() != 0){
+cout << "Do you wish to remove an element in List 1? (y/n)";
+cin >> ans;
 
+while( ans == 'y' && L2.size() != 0){
+cout << "\n";
 cout << "Please enter the position of the element you wish to remove in List 2(an integer)";
 cin >> rm;
 
 L1.remove(rm);
+cout << "\n";
 
 cout << "List 1: ";
 L2.display();
-cout << "size: " << L2.size();
+cout << "\n";
+cout << "size: " << L2.size() << "\n";
 
 cout << "Do you wish to remove another element in List 2? (y/n)";
 cin >> ans;
 }
 
 
-cout << "The first element of List 1 is" <<  L1.getAt(1);
-cout << "The second element of List 1 is" <<  L1.getAt(2);
+cout << "The first element of List 1 is" <<  L1.getAt(1) << "\n";
+cout << "The second element of List 1 is" <<  L1.getAt(2) << "\n";
 
 }
