82f11cd3b304dd60ac6c0be7152955f9eaa417d0
RainCoder <jyolainnie_daniel@hotmail.com>
2017-03-18 05:37:22 -0400

    Added method implementation

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,36 @@
 #include "StackV.h"
+#include <iostream>
+using namespace std;
+
+
+
+int Stack::size()
+{
+  data.size();
+}
+
+void Stack::push(int val)
+{
+	
+ data.push_back(val);
+ 
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+int Stack::top()
+{
+	return data.[size()-1];
+	
+}
+
+void Stack::clear()
+{
+	while(data.size() != 0)
+	 pop();
+}
+
+
