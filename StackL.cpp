501280b27b94e4378f87e86aee984c026eced4df
RainCoder <jyolainnie_daniel@hotmail.com>
2017-03-18 05:21:42 -0400

    Added implementation of methods

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,34 @@
 #include "StackL.h"
+#include <iostream>
+using namespace std;
+
+
+
+int Stack::size()
+{
+  data.size();
+}
+
+void Stack::push(int val)
+{
+ k = 1;
+data.insert(val,k);
+}
+
+void Stack::pop()
+{
+k = 1;
+data.remove(k);
+}
+
+int Stack::top()
+{
+	data.display();
+	
+}
+
+void Stack::clear()
+{
+data.clear();
+}
+
